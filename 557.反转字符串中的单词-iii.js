d085975e0804ecd47bdedbdb2e8887a72fea02db
CodeGorgeous <2460481461@qq.com>
2021-08-15 20:33:56 +0800

    557新增解题方法

--- 557.反转字符串中的单词-iii.js
+++ 557.反转字符串中的单词-iii.js
@@ -10,22 +10,26 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let a = s.split(' ')
-    let i = 0
-    while (i<a.length) {
-        let s = a[i].split('')
-        let l = 0
-        let e = s.length - 1
-        while (l<e) {
-            let x = s[l]
-            s[l] = s[e]
-            s[e] = x
-            l++
-            e--
-        }
-        a[i] = s.join('')
-        i++
-    }
-    return a.join(' ')
+    // 基础版
+    // let a = s.split(' ')
+    // let i = 0
+    // while (i<a.length) {
+    //     let s = a[i].split('')
+    //     let l = 0
+    //     let e = s.length - 1
+    //     while (l<e) {
+    //         let x = s[l]
+    //         s[l] = s[e]
+    //         s[e] = x
+    //         l++
+    //         e--
+    //     }
+    //     a[i] = s.join('')
+    //     i++
+    // }
+    // return a.join(' ')
+    // 借鉴别人思路的奇思妙想懒人版
+    let a = s.split('').reverse().join('')
+    return a.split(' ').reverse().join(' ')
 };
 // @lc code=end
